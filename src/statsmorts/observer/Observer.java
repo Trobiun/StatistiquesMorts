806f0e86b27e40a63f8701c47f67438c8fdc1d28
robin <robin@serdaigle.poudlard>
2017-12-07 21:24:41 +0100

    Ajout des déclarations des méthodes pour supprimer les éléments graphiques qui représentent des plateformes, genres, studios etc

--- src/statsmorts/observer/Observer.java
+++ src/statsmorts/observer/Observer.java
@@ -12,7 +12,7 @@ import statsmorts.classes.Live;
 import statsmorts.classes.Plateforme;
 import statsmorts.classes.Run;
 import statsmorts.classes.Studio;
-import statsmorts.classes.TypeRacine;
+import statsmorts.classes.TypeGroup;
 
 /**
  *
@@ -20,14 +20,21 @@ import statsmorts.classes.TypeRacine;
  */
 public interface Observer {
     
-    public void clear(TypeRacine type);
+    public void clear(TypeGroup type);
     public void addPlateforme(Plateforme plateforme);
     public void addGenre(Genre genre);
     public void addStudio(Studio studio);
     public void addJeu(Jeu jeu);
     public void addRun(long idJeu, Run run);
     public void addLive(long idRun, Live live);
+    public void removePlateforme(long idPlateforme);
+    public void removeGenre(long idGenre);
+    public void removeStudio(long idStudio);
+    public void removeJeu(long idJeu);
+    public void removeRun(long idRun);
+    public void removeLive(long idLive);
     public void updateDataset(String titre, DefaultCategoryDataset dataset);
     public void fillPlateforme(long idPlateforme, String nomPlateforme);
+    public void fillGenre(long idGenre, String nom);
     
 }
