b40336b6a7e7c456ef2a7e826275bdc922fb5534
robin <robin@serdaigle.poudlard>
2017-12-07 21:26:00 +0100

    Ajout et modification des méthodes pour notifier l'ajout ou la suppression des éléments graphqiques qui représentent des plateformes, des genres, studios etc

--- src/statsmorts/observer/Observable.java
+++ src/statsmorts/observer/Observable.java
@@ -12,7 +12,7 @@ import statsmorts.classes.Live;
 import statsmorts.classes.Plateforme;
 import statsmorts.classes.Run;
 import statsmorts.classes.Studio;
-import statsmorts.classes.TypeRacine;
+import statsmorts.classes.TypeGroup;
 
 
 /**
@@ -22,14 +22,21 @@ import statsmorts.classes.TypeRacine;
 public interface Observable {
     
     public void setObserver(Observer observer);
-    public void setTypeRacine(TypeRacine type);
-    public void notifyPlateforme(Plateforme plateforme);
-    public void notifyGenre(Genre genre);
-    public void notifyStudio(Studio studio);
-    public void notifyJeu(Jeu jeu);
-    public void notifyRun(Run run);
-    public void notifyLive(Live live);
+    public void setGroup(TypeGroup type);
+    public void notifyAddPlateforme(Plateforme plateforme);
+    public void notifyAddGenre(Genre genre);
+    public void notifyAddStudio(Studio studio);
+    public void notifyAddJeu(Jeu jeu);
+    public void notifyAddRun(Run run);
+    public void notifyAddLive(Live live);
+    public void notifyRemovePlateforme(long idPlateforme);
+    public void notifyRemoveGenre(long idGenre);
+    public void notifyRemoveStudio(long idStudio);
+    public void notifyRemoveJeu(long idJeu);
+    public void notifyRemoveRun(long idRun);
+    public void notifyRemoveLive(long idLive);
     public void notifyDataset(String titre, DefaultCategoryDataset dataset);
     public void notifyFillPlateforme(long idPlateforme);
+    public void notifyFillGenre(long idGenre);
     
 }
