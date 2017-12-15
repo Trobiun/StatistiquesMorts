dc6ad85a7eb131234dd654c628f48cb24d2e654d
robin <robin@serdaigle.poudlard>
2017-12-15 20:18:46 +0100

    Ajout de l'énumération ModeGestion qui permet de différencier une action d'ajout, de modification ou de suppression

---
+++ src/statsmorts/vue/ModeGestion.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statsmorts.vue;
+
+import constantes.TexteConstantes;
+
+/**
+ *
+ * @author robin
+ */
+public enum ModeGestion {
+    
+    //ÉNUMÉRATION
+    AJOUTER(TexteConstantes.AJOUTER),
+    MODIFIER(TexteConstantes.MODIFIER),
+    SUPPRIMER(TexteConstantes.SUPPRIMER);
+    
+    
+    //ATTRIBUTS
+    private final String action;
+    
+    
+    //CONSTRUCTEUR
+    private ModeGestion(String action) {
+        this.action = action;
+    }
+    
+    
+    //ACCESSEURS
+    public String getAction() {
+        return action;
+    }
+}
