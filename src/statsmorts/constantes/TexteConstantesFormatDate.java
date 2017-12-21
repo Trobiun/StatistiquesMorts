6b549a67fb44f57c99833595c6d1b194d5571c29
robin <robin@serdaigle.poudlard>
2017-12-21 21:35:55 +0100

    Ajout des constantes texte pour les pattern de dates

---
+++ src/statsmorts/constantes/TexteConstantesFormatDate.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statsmorts.constantes;
+
+/**
+ *
+ * @author Robin
+ */
+public class TexteConstantesFormatDate {
+    
+    public static final String SQL = "yyyy-MM-dd HH:mm:SS";
+    public static final String LONG = "dd/MM/yyyy HH:mm";
+    public static final String SHORT = "dd/MM/yyyy";
+    public static final String YEAR_ONLY = "yyyy";
+    
+}
