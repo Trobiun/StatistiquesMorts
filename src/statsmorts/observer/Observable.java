b3ccb4da199637f9965a3aa8d035f6d1a4d70724
robin <robin@serdaigle.poudlard>
2017-12-22 17:13:14 +0100

    Création de javadoc.

--- src/statsmorts/observer/Observable.java
+++ src/statsmorts/observer/Observable.java
@@ -16,37 +16,130 @@ import statsmorts.classes.TypeGroup;
 
 
 /**
- *
+ * Une interface pour l'architecture MVC.
  * @author Robin
  */
 public interface Observable {
     
+    /**
+     * Change l'observer par oobserver.
+     * @param observer le nouvel observer.
+     */
     void setObserver(Observer observer);
+    /**
+     * Change le regroupement des jeux par le type "type"
+     * @param type le nouveau type par lequel regrouper les jeux
+     */
     void setGroup(TypeGroup type);
     
     //notifications de la base de données
+    /**
+     * Notifie l'observer d'ajouter la plateforme "plateforme" à l'affichage.
+     * @param plateforme la plateforme à ajouter.
+     */
     void notifyAddPlateforme(Plateforme plateforme);
+    /**
+     * Notifie l'observer d'ajouter le genre "genre" à l'affichage.
+     * @param genre le genre à ajouter.
+     */
     void notifyAddGenre(Genre genre);
+    /**
+     * Notifie l'observer d'ajouter le studio "studio" à l'affichage.
+     * @param studio le studio à ajouter
+     */
     void notifyAddStudio(Studio studio);
+    /**
+     * Notifie l'observer d'ajouter le jeu "jeu" à l'affichage.
+     * @param jeu le jeu à ajouter.
+     */
     void notifyAddJeu(Jeu jeu);
+    /**
+     * Notifie l'observer d'ajouter la run "run" à l'affichage.
+     * @param run la run à ajouter.
+     */
     void notifyAddRun(Run run);
+    /**
+     * Notifie l'observer d'ajouter le live "live" à l'affichage.
+     * @param live le live à ajouter.
+     */
     void notifyAddLive(Live live);
+    /**
+     * Notifie l'observer de supprimer la plateforme "plateforme" à l'affichage.
+     * @param idPlateforme l'identifiant de la plateforme à supprimer.
+     */
     void notifyRemovePlateforme(long idPlateforme);
+    /**
+     * Notifie l'observer de supprimer le genre "genre" à l'affichage.
+     * @param idGenre l'identifiant du genre à supprimer.
+     */
     void notifyRemoveGenre(long idGenre);
+    /**
+     * Notifie l'observer de supprimer le studio "studio" à l'affichage.
+     * @param idStudio l'identifiant du studio à supprimer.
+     */
     void notifyRemoveStudio(long idStudio);
+    /**
+     * Notifie l'observer de supprimer le jeu "jeu" à l'affichage.
+     * @param idJeu l'identifiant du jeu à supprimer.
+     */
     void notifyRemoveJeu(long idJeu);
+    /**
+     * Notifie l'observer de supprimer la run "run" à l'affichage.
+     * @param idRun l'identifiant de la run à supprimer.
+     */
     void notifyRemoveRun(long idRun);
+    /**
+     * Notifie l'observer de supprimer le lvie "live" à l'affichage.
+     * @param idLive l'identifiant du live à supprimer.
+     */
     void notifyRemoveLive(long idLive);
-    
+    /**
+     * Notifie l'observer que le dataset a changé et doit le mettre à jour.
+     * @param titre le titre du graphique (chart) à afficher.
+     * @param dataset les données du graphqiue à afficher.
+     */
     void notifyDataset(String titre, DefaultCategoryDataset dataset);
     
     //notifications pour remplir les panels d'entrées utilisateur
+    /**
+     * Notifie l'observer de remplir les panels de saisie de plateformes.
+     * @param idPlateforme l'identifiant de la plateforme à remplir.
+     */
     void notifyFillPlateforme(long idPlateforme);
+    /**
+     * Notifie l'observer de remplir les panels de saisie de genres.
+     * @param idGenre l'identifiant du genre à remplir.
+     */
     void notifyFillGenre(long idGenre);
+    /**
+     * Notifie l'observer de remplir les panels de saisie de studios.
+     * @param idStudio l'identifiant du studio à remplir.
+     */
     void notifyFillStudio(long idStudio);
+    /**
+     * Notifie l'observer de remplir les panels de saisie de jeux.
+     * @param idJeu l'identifiant du jeu à remplir.
+     */
     void notifyFillJeu(long idJeu);
+    /**
+     * Notifie l'observer de remplir les panels de saisie de runs.
+     * @param idRun l'identifiant de la run à remplir.
+     */
     void notifyFillRun(long idRun);
+    /**
+     * Notifie l'observer de remplir les panels de jeux dans les panels de saisie de runs.
+     * @param idJeu l'identifiant du jeu à remplir.
+     */
     void notifyFillRunJeu(long idJeu);
+    /**
+     * Notifie l'observer de remplir les panels de saisie de lives.
+     * @param idLive l'identifiant du live à remplir.
+     */
     void notifyFillLive(long idLive);
+    /**
+     * Notifie l'observer de remplir les panels de runs dans les panels de saisie de lives.
+     * @param idRun l'identifiant de la run à remplir.
+     */
+    void notifyFillLiveRun(long idRun);
     
 }
--- src/statsmorts/observer/Observer.java
+++ src/statsmorts/observer/Observer.java
@@ -16,35 +16,140 @@ import statsmorts.classes.Studio;
 import statsmorts.classes.TypeGroup;
 
 /**
- *
+ * Une interface pour l'architecture MVC.
  * @author Robin
  */
 public interface Observer {
     
     //méthodes pour mettre à jour la vue en fonction du modèle
+    /**
+     * Vide tous les arbres et les panels de saisies d'utilisateur.
+     * @param type le type par lequel grouper (pour reset le nœud root).
+     */
     void clear(TypeGroup type);
+    /**
+     * Ajoute une plateforme à l'affichage.
+     * @param plateforme la plateforme à ajouter.
+     */
     void addPlateforme(Plateforme plateforme);
+    /**
+     * Ajoute un genre à l'affichage.
+     * @param genre le genre à ajouter.
+     */
     void addGenre(Genre genre);
+    /**
+     * Ajoute un studio à l'affichage.
+     * @param studio le studio à ajouter
+     */
     void addStudio(Studio studio);
+    /**
+     * Ajoute un jeu à l'affichage.
+     * @param jeu le jeu à ajouter.
+     */
     void addJeu(Jeu jeu);
-    void addRun(long idJeu, Run run);
-    void addLive(long idRun, Live live);
+    /**
+     * Ajoute une run à l'affichage. 
+     * @param run la run à ajouter.
+     */
+    void addRun(Run run);
+    /**
+     * Ajoute un live à l'affichage
+     * @param live le live à ajouter.
+     */
+    void addLive(Live live);
+    /**
+     * Supprime une plateforme de l'affichage.
+     * @param idPlateforme l'identifiant de la plateforme à supprimer.
+     */
     void removePlateforme(long idPlateforme);
+    /**
+     * Supprime un genre de l'affichage.
+     * @param idGenre l'identifiant du genre à supprimer.
+     */
     void removeGenre(long idGenre);
+    /**
+     * Supprime un studio de l'affichage.
+     * @param idStudio l'identifiant du studio à supprimer.
+     */
     void removeStudio(long idStudio);
+    /**
+     * Supprime un jeu de l'affichage.
+     * @param idJeu l'identifiant du jeu à supprimer.
+     */
     void removeJeu(long idJeu);
+    /**
+     * Supprime une run de l'affichage.
+     * @param idRun l'identifiant de la run à supprimer.
+     */
     void removeRun(long idRun);
+    /**
+     * Supprime un live de l'affichage.
+     * @param idLive l'identifiant du live à supprimer.
+     */
     void removeLive(long idLive);
+    
+    /**
+     * Met à jour le dataset pour le graphique à afficher.
+     * @param titre le titre du graphique.
+     * @param dataset les données du graphique à afficher.
+     */
     void updateDataset(String titre, DefaultCategoryDataset dataset);
     
     //méthodes pour remplir les champs de saisie pour les entrées utilisateur
+    /**
+     * Remplit les panels de saisies utilisateur pour les plateformes.
+     * @param nomPlateforme le nom de la plateforme à afficher dans les champs
+     *                      de saisie utilisateur.
+     */
     void fillPlateforme(String nomPlateforme);
+    /**
+     * Remplit les panels de saisies utilisateur pour les genres.
+     * @param nomGenre 
+     */
     void fillGenre(String nomGenre);
+    /**
+     * Remplit les panels de saisies utilisateur pour les studio.
+     * @param nomStudio 
+     */
     void fillStudio(String nomStudio);
+    /**
+     * Remplit les panels de saisies utilisateur pour les jeux.
+     * @param titreJeu
+     * @param anneeSortie
+     * @param listPlateformes
+     * @param listGenres
+     * @param idStudio 
+     */
     void fillJeu(String titreJeu, int anneeSortie, Long[] listPlateformes, Long[] listGenres, long idStudio);
+    /**
+     * Remplit les panels de saisies utilisateur pour les runs.
+     * Remplit les champs de saisies directement liés à la run.
+     * @param titreRun
+     * @param idJeu 
+     */
     void fillRun(String titreRun, long idJeu);
+    /**
+     * Remplit les panels de saisies utilisateur pour les runs.
+     * Remplit les champs de saisies liés au jeu de la run.
+     * @param titreJeu 
+     */
     void fillRunJeu(String titreJeu);
-    void fillLive(Date dateDebut, Date dateFin, int morts);
-    void fillLiveRun(long idRun, String titreRun);
+    /**
+     * Remplit les panels de saisies utilisateur pour les lives.
+     * Remplit les champs de saisies directement liés au live.
+     * @param idRun
+     * @param dateDebut
+     * @param dateFin
+     * @param morts 
+     */
+    void fillLive(long idRun, Date dateDebut, Date dateFin, int morts);
+    /**
+     * Remplit les panels de saisies utilisateur pour les lives.
+     * Remplit les champs de saisies liés à la run du live.
+     * @param idRun
+     * @param titreRun
+     * @param titreJeu 
+     */
+    void fillLiveRun(long idRun, String titreRun, String titreJeu);
     
 }
