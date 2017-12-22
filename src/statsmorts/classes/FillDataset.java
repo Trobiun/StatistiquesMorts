effae7bd9cfbf4ba1c115c5aadc69f8656cdb390
robin <robin@serdaigle.poudlard>
2017-12-22 17:22:07 +0100

    Création de javadoc.

--- src/statsmorts/classes/FillDataset.java
+++ src/statsmorts/classes/FillDataset.java
@@ -10,14 +10,38 @@ import java.util.concurrent.TimeUnit;
 import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
- *
+ * Une interface pour remplit le Dataset d'un graphique (charrt) pour l'interface
+ * graphique
  * @author Robin
  */
 public interface FillDataset {
     
-    public String getTitre();
-    public String getTitreDataset();
-    public ArrayList<Live> getLivesList();
-    public void fillDataset(DefaultCategoryDataset dataset, TimeUnit unit, boolean total);
+    /**
+     * Retourne le titre de l'objet qui implémente cette interface.
+     * @return une chaîne de caractères égale au titre de l'objet qui implémente cette interface.
+     */
+    String getTitre();
+    /**
+     * Retourne le titre à affciher du Dataset pour le graphique dans l'interface
+     * graphique.
+     * @return une chaîne de caractères
+     */
+    String getTitreDataset();
+    /**
+     * Retourne une arraylist composée de tous les lives contenus dans l'objet qui
+     * implémente cette interface.
+     * @return une arraylist qui contient les lives
+     */
+    ArrayList<Live> getLivesList();
+    /**
+     * Remplit le dataset 
+     * @param dataset le dataset à remplir.
+     * @param unit l'unité de temps utilisée pour les calculs de durée totale
+     *             ou pour la durée de vie moyenne.
+     * @param total booléen qui sert à déterminer si l'objet qui implémente cette
+     *              interface doit donner le total des statistiques (faire la somme
+     *              des morts, des durées, des durées de vie etc).
+     */
+    void fillDataset(DefaultCategoryDataset dataset, TimeUnit unit, boolean total);
     
 }
