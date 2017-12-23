21f96e41307e338304718abe868ed340cb5fc606
robin <robin@serdaigle.poudlard>
2017-12-23 14:38:33 +0100

    Ajout des constantes de conversion pour les requêtes préparées avec la méthode preparedSetParameters

---
+++ src/statsmorts/constantes/TexteConstantesConnexion.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package statsmorts.constantes;
+
+/**
+ * Une classe pour les constantes de conversion de string utilisée avec les 
+ * requêtes préparées.
+ * @author Robin
+ */
+public final class TexteConstantesConnexion {
+    
+    //CONSTANTES DE CONVERSION
+    public static final String BOOL = "(bool)";
+    public static final String INT = "(int)";
+    public static final String LONG = "(long)";
+    
+    
+    //CONSTRUCTEUR
+    private TexteConstantesConnexion() { }
+}
