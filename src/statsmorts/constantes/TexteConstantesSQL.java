750deae540dbd8ba20fa3c23e4997013e36610bd
robin <robin@serdaigle.poudlard>
2017-12-23 14:31:35 +0100

    Ajout des constantes SELECT, FROM et IN.

--- src/statsmorts/constantes/TexteConstantesSQL.java
+++ src/statsmorts/constantes/TexteConstantesSQL.java
@@ -85,12 +85,15 @@ public final class TexteConstantesSQL {
                                                             + TABLE_RUNS_ID_JEU + " = " + TABLE_JEUX_ID;
     
     //GESTION DE BASE DE DONNÉES (INSERT_INTO, UPDATE, DELETE_FROM)
+    public static final String SELECT = "SELECT";
+    public static final String FROM = "FROM";
     public static final String INSERT_INTO = "INSERT INTO";
     public static final String UPDATE = "UPDATE";
     public static final String DELETE = "DELETE";
     public static final String DELETE_FROM = "DELETE FROM";
     public static final String VALUES = "VALUES";
     public static final String SET = "SET";
+    public static final String IN = "IN";
     public static final String WHERE = "WHERE";
     public static final String ON = "ON";
     public static final String NULL = "NULL";
